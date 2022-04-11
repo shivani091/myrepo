9d9e9db317afb6f48205462cb6ebcd1570fd6afa
shivani091 <103453716+shivani091@users.noreply.github.com>
2022-04-12 00:29:26 +0530

    Add files via upload

---
+++ storage1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int x = 51;
+
+void demo()
+{
+	int no = 11;
+	auto int data = 21;
+	int y;
+	register int a =51;
+	
+}
+
+int main()
+{
+	int i=10;
+	auto int j = 20;
+	
+	demo();
+	
+	return 0;
+}
